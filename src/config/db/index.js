3b655c786ec5747c0d7fd99bfb52ad7d6ec1d650
thanhhau <trannguyenthanhhaupro09@gmail.com>
2024-04-24 22:13:24 +0700

    connect db mongo alat

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 async function conn() {
     try {
         const url = "mongodb+srv://phandinhphuoc02:dadZprcUWLzCWqiZ@cluster0.u4fxy74.mongodb.net/Moto?retryWrites=true&w=majority&appName=Cluster0"
-        // await mongoose.connect("mongodb://localhost:27017/Motos")
+        await mongoose.connect(url)
         console.log('Connected to Motos database')
     } catch (error) {
         console.log('Error connecting to Motos database')
